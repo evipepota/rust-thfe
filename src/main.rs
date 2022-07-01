6d6c498837c1a8c454375e73930f2d6c73eca3a2
evipepota <meimuyuta@gmail.com>
2022-07-02 00:38:33 +0900

    main

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,11 @@ mod tlwe;
 mod trlwe;
 mod calc;
 use calc::complex;
-use tlwe::test;
 
 //type Torus = u32;
 
 fn main() {
-    test(1);
+    tlwe::test(1);
+    tlwe::test(0);
+    trlwe::test();
 }
