4bc1cbeaf60efbe8d2dc5db29ddb88ad55eacc07
evipepota <meimuyuta@gmail.com>
2022-07-01 20:54:38 +0900

    trlwe パラメータの変更

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,10 @@ mod tlwe;
 mod trlwe;
 mod calc;
 use calc::complex;
-use crate::trlwe::a;
 use tlwe::test;
 
 //type Torus = u32;
 
 fn main() {
     test(1);
-    a();
 }
--- src/trlwe.rs
+++ src/trlwe.rs
@@ -2,14 +2,5 @@ use crate::calc::{fft, complex::{Complex, self}};
 
 type Torus = u32;
 const ALPHA: f64 = 1.0 / 2i64.pow(25) as f64;
-pub const N: usize = 1024;
-
-pub fn a() {
-    let b = Complex{
-        re: 1.0,
-        im: 1.0
-    };
-    let mut c : [Complex; N] = [b; N];
-    let mut d : [Complex; N] = [b; N];
-    fft::convolution(&mut c, &mut d);
-}
+pub const N: usize = 512;
+pub const K: usize = 2;
